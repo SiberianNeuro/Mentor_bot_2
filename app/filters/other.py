d631b7568ddaa575381c4eea6fdd7d1708ab30dd
Nikita Kanushin <kanushinn@gmail.com>
2022-07-04 17:12:53 +0300

    mysql bugfix

--- app/filters/other.py
+++ app/filters/other.py
@@ -1,12 +1,12 @@
-from app.db.mysql_db import chat_id_check
+from app.db.mysql_db import chat_id_check, admins_ids
 
 
 async def is_register(obj):
     result = await chat_id_check()
-    if result is None:
-        return False
+    admins = await admins_ids()
+    if obj is admins:
+        return True
+    elif obj in result:
+        return True
     else:
-        if obj in result:
-            return True
-        else:
-            return False
+        return False
